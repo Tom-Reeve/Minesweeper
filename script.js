2f0747964d9675ba12cd65f4aadafad284773bed
Tom Reeve <thomas.reeve.tr@gmail.com>
2025-05-23 23:18:34 +0100

    Changed button display

--- script.js
+++ script.js
@@ -1,14 +1,20 @@
+const newGameButton = document.getElementById("newGameButton");
+const startButton = document.getElementById("startGameButton");
+
 const difficultyRadios = document.querySelectorAll(".hiddenL1");
 const customRadio = document.getElementById("customRadio");
 
 const customInput = document.querySelectorAll(".hiddenL2");
 
 function newGame() {
+    newGameButton.disabled = true;
+    startButton.disabled = true;
     for (let div of difficultyRadios) {
         div.style.display = "flex";
     }
     difficultyRadios.forEach(radio => {
         radio.addEventListener("change", () => {
+            startButton.disabled = false;
             if (customRadio.checked) {
                 for (let div of customInput) {
                     div.style.display = "flex";
